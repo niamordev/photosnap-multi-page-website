ed9670e14c7f6af2711278c6e1ae7b73f2a2ccb3
NiamorDev <niamordev@proton.me>
2022-12-19 10:26:54 +0100

    add nav mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  const [showNavMobile, setShowNavMobile] = useState(false);
+
+  const handleNavMobile = () => {
+    setShowNavMobile(!showNavMobile);
+    document.querySelector('#html').classList.toggle('overflow__hidden')
+  };
   return (
     <header>
       <nav>
@@ -15,8 +21,27 @@ export default function Header() {
         <img
           src="../photosnap-multi-page-website/assets/images/shared/mobile/menu.svg"
           alt=""
+          onClick={() => handleNavMobile()}
           className="mobile"
         />
+        {showNavMobile ? <div className="overlay"></div> : null}
+        {showNavMobile ? (
+          <div className="navbar__mobile__menu">
+            <ul>
+              <NavLink to="/photosnap-multi-page-website/stories">
+                <li>STORIES</li>
+              </NavLink>
+              <NavLink to="/photosnap-multi-page-website/features">
+                <li>FEATURES</li>
+              </NavLink>
+              <NavLink to="/photosnap-multi-page-website/pricing">
+                <li>PRICING</li>
+              </NavLink>
+            </ul>
+            <hr />
+            <button>GET AN INVITE</button>
+          </div>
+        ) : null}
 
         <ul className="nav-big-screen">
           <NavLink to="/photosnap-multi-page-website/stories">
